27a8b0a4ba3e7cef90a5122e81d4b09f70ccc5e1
Dewan Minhajul Islam <dewan.minhajul@gmail.com>
2026-09-23 09:18:18 +0600

    Database Create using Model

--- Inventory_Project/products/models.py
+++ Inventory_Project/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class ProductModel(models.Model):
+    
+    name = models.CharField(max_length=100)
+    
+    description = models.TextField()
+    
+    price = models.PositiveIntegerField()
+    
+    production_date = models.DateField()
+    
+    def __str__(self):
+        return f'{self.name} - {self.price}'
